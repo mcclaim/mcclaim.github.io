3af0c1937b2051d390db27ed5f8ca29e616da535
Yummy <farrux.9522s@gmail.com>
2017-10-27 17:53:57 +0500

    personal site for Lena

---
+++ project-lena/js/main.js
@@ -0,0 +1,4 @@
+function setVideo() {
+	$('.inform_video a.play').remove();
+    $('#video_wrap').html('<iframe src="https://www.youtube.com/embed/nRTJd91G04Q?modestbranding=1;rel=0;autohide=3;controls=1;autoplay=1;showinfo=3;iv_load_policy=3;" frameborder="0" allowfullscreen></iframe>');
+}
