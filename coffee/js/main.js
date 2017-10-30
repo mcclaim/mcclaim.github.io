2fb3a8e5f336cb543a9118e43ac57ce878d4b1bd
Yummy <farrux.9522s@gmail.com>
2017-10-30 12:56:06 +0500

    Add js functions

--- coffee/js/main.js
+++ coffee/js/main.js
@@ -1,16 +1,55 @@
 $(document).ready(function() {
 	// Ajax отправка почты с сайта
-	$("form").submit(function() { //устанавливаем событие отправки для формы с id=form
-            var form_data = $(this).serialize(); //собераем все данные из формы
-            $.ajax({
-            type: "POST", //Метод отправки
-            url: "send.php", //путь до php фаила отправителя
-            data: form_data,
-            success: function() {
-                   //код в этом блоке выполняется при успешной отправке сообщения
-                   alert("Ваше сообщение отпрвлено!");
-            });
+	$("form").submit(function(){ // перехватываем все при событии отправки
+        var form = $(this); // запишем форму, чтобы потом не было проблем с this
+        var error = false; // предварительно ошибок нет
+        if (!error) { // если ошибки нет
+            var data = form.serialize(); // подготавливаем данные
+            $.ajax({ // инициализируем ajax запрос
+               type: 'POST', // отправляем в POST формате, можно GET
+               url: 'send.php', // путь до обработчика, у нас он лежит в той же папке
+               data: data, // данные для отправки
+               beforeSend: function(data) { // событие до отправки
+                    form.find('input[type="submit"]').attr('disabled', 'disabled'); // например, отключим кнопку, чтобы не жали по 100 раз
+                  },
+               success: function(data){ // событие после удачного обращения к серверу и получения ответа
+                    if (data['error']) { // если обработчик вернул ошибку
+                        alert(data['error']); // покажем её текст
+                    } else { // если все прошло ок
+                        alert('Ваше письмо отправлено'); // пишем что все ок
+                    }
+                 },
+               error: function (xhr, ajaxOptions, thrownError) { // в случае неудачного завершения запроса к серверу
+                    alert(xhr.status); // покажем ответ сервера
+                    alert(thrownError); // и текст ошибки
+                 },
+               complete: function(data) { // событие после любого исхода
+                    form.find('input[type="submit"]').prop('disabled', false); // в любом случае включим кнопку обратно
+                 }
+                          
+                 });
+        }
+        return false; // вырубаем стандартную отправку формы
     });
+
+    // Проверка на цифру
+	    $("input[name='phone']").keydown(function(event) {
+	        // Разрешаем нажатие клавиш backspace, Del, Tab и Esc
+	        if ( event.keyCode == 46 || event.keyCode == 107 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 ||
+	             // Разрешаем выделение: Ctrl+A
+	            (event.keyCode == 65 && event.ctrlKey === true) ||
+	             // Разрешаем клавиши навигации: Home, End, Left, Right
+	            (event.keyCode >= 35 && event.keyCode <= 39)) {
+	                 return;
+	        }
+	        else {
+	            // Запрещаем всё, кроме клавиш цифр на основной клавиатуре, а также Num-клавиатуре
+	            if ((event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+	                event.preventDefault();
+	            }  
+	        }
+	    });
+
 });
 function setVideo() {
 	$('.inform_video a.play').remove();
