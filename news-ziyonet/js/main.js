d9cb86f878027769749489119f8884564169f6bc
TriSTaR95 <farrux.9522s@gmail.com>
2018-04-26 00:26:18 +0500

    main page news.ziyonet.uz

---
+++ news-ziyonet/js/main.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+
+    // Video block
+    $('.video_items').slick({
+      dots: false,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+
+    $('.video_items .video_item').each(function(index, element) {
+      var href = $(element).find('.video_item_img a').attr('href');
+      $(element).magnificPopup({
+        items: {
+          src: href
+        },
+        type: 'iframe'
+      });
+    });
+
+    var a = new StickySidebar('.category_block', {
+      topSpacing: 0,
+      bottomSpacing: 20,
+      containerSelector: '.aside_cont',
+      innerWrapperSelector: '.category_block_sticky'
+    });
+
+  // Header block
+  var $window = $(window),
+      $card = $('.must_see_wrap'),
+      $whenNot = $('.must_see_wrap .slick-list'),
+      toggleSlick;
+
+    toggleSlick = function () {
+        if ($window.width() <= 480) {
+            $card.slick();
+        } else {
+            $card.slick('unslick');
+        }
+    }
+    toggleSlick2 = function () {
+        if ($window.width() <= 480) {
+            $card.slick();
+        } else {
+            if ($whenNot.length) {
+              $card.slick('unslick');
+            }
+        }
+    }
+    $window.resize(toggleSlick);
+    toggleSlick2();
+
+});
