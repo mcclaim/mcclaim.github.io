6068903c839c9ca68d46968212274c82f21a3b21
Volonter <volonter@uzinfocom.local>
2018-05-01 17:22:43 +0500

    New menu

--- news-ziyonet/menu/js/main.js
+++ news-ziyonet/menu/js/main.js
@@ -4,13 +4,13 @@ $(function () {
 	$(document).on('click touch', '.navbar_btn', function(event) {
 		event.preventDefault();
   		$(this).closest('.navbar').find('ul').addClass('open_menu');
-  		$('body').append('<div class="menu_close"></div>');
+  		$('.navbar').append('<div class="menu_close"></div>');
 	});
 
 	$(document).on('click touch', '.menu_close', function(event) {
 		event.preventDefault();
-  		$(this).closest('body').find('.open_menu').removeClass('open_menu');
-  		$('body').find('.menu_close').remove();
+  		$(this).closest('.navbar').find('ul').removeClass('open_menu');
+  		$(this).remove();
 	});
 
 })
